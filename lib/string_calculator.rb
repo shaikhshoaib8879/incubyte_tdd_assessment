ebe4056ba7783189265c4a5d751e6cec35fbb7bf
beingshoaib <shoaib.shaikh@gethownow.com>
2025-09-05 20:42:27 +0530

    updated method where it returns sum of given numbers in string and also added test case , negative test case is commented

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,5 +3,8 @@ class StringCalculator
 
       def self.add(input)
         return 0 if input.empty?
+
+        numbers = input.split(',')
+        numbers.map(&:to_i).sum
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,8 +3,22 @@ require_relative "../lib/string_calculator"
 
 RSpec.describe StringCalculator do
   describe "adding numbers from a string" do
-    it "returns 0 when input is empty" do
+    it "returns 0 when input is empty " do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "return given number when single number is given" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    #failing test case
+    # it "return incorrect sum of two numbers when provided" do
+    #   expect(StringCalculator.add("1,3")).to eq(2)
+    # end
+
+    it "return incorrect sum of two numbers when provided" do
+      expect(StringCalculator.add("1,3")).to eq(4)
+    end
+
   end
 end
